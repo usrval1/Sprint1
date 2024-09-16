3c2d15f33c1adf83a37ae73c08f6d62b154dceff
Toctillo <202210072@urp.edu.pe>
2024-09-16 01:54:02 -0500

    avance validacion

--- front/src/Schemas/AUTH.js
+++ front/src/Schemas/AUTH.js
@@ -1,6 +1,15 @@
 import {z}   from "zod";
 export const loginSchema = z.object({
-    email:z.string().min(1, { message: 'Tienes que colocar tu email!!!!' }),
-    password:z.string()//crear las validaciones
+    email: z.string()
+    .min(1, { message: 'No ha ingresado el email' }) // El email no puede estar vacío
+    .email({ message: 'El formato del email es incorrecto, debe contener un @ y un dominio.' }), // Validar formato de email
+   
+  password: z.string()
+    .min(1, { message: 'No ha ingresado la contraseña' }) // La contraseña no puede estar vacía
+    .min(6, { message: 'La contraseña es incorrecta' }) // Validar longitud mínima de la contraseña
+    
+
+
+    
     
 });
