d88ec35c856854d17742aec517c514614a0af7a8
Nicholas Altamirano <nicholas0214.na@gmail.com>
2024-09-19 15:01:27 -0500

    Correcciones de Nicholas_Altamirano
    Mejoras MenuSidebar. Añadidos los jsx: GestionarUsuarios y LoggedUsuario (este ultimo una especie de recibimiento para el usuario que ingresa a través del login)

---
+++ front/src/pages/Usuario/LoggedUsuario.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import MenuSideBar from '../../components/MenuSideBar';
+import NavBar from '../../components/NavBar';
+
+export function LoggedUsuario() {
+  const [drawerOpen, setDrawerOpen] = useState(true);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <MenuSideBar open={drawerOpen} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3, ml: drawerOpen ? 300 : 70 }}>
+        <NavBar onDrawerToggle={handleDrawerToggle} drawerOpen={drawerOpen} />
+        <Box sx={{ mt: 8 }}>
+          <div>Usuario logeado con éxito!</div>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default LoggedUsuario;
